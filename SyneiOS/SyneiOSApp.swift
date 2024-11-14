b779aa689442d231010831e0cdf74ec006324dfe
iexcalibur <developer@finallyfreeproductions.com>
2024-11-14 19:53:19 +0530

    Initial Commit

---
+++ SyneiOS/SyneiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyneiOSApp.swift
+//  SyneiOS
+//
+//  Created by Shubham on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SyneiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
